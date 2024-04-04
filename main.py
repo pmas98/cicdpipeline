44f3b7084e79ba35b615f17768ae6d5f560043b9
pmas98 <pmas98@hotmail.com>
2024-04-04 11:52:30 -0300

    Add .env and profile.html to .gitignore, update requirements.txt with watchtower, and add logging and CloudWatch integration

--- main.py
+++ main.py
@@ -6,8 +6,28 @@ from app.middleware import PyInstrumentMiddleWare
 import dotenv
 import os
 import json
+import watchtower
+import logging
+from time import strftime
+import boto3
 
 dotenv.load_dotenv()
+
+boto3.Session(
+    region_name=os.environ.get("AWS_DEFAULT_REGION"),
+    aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
+)
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.INFO)
+console_handler = logging.StreamHandler()
+cw_handler = watchtower.CloudWatchLogHandler(
+    log_group="identity",
+    stream_name=strftime("%Y-%m-%d-%H-%M-%S")
+)
+LOGGER.addHandler(console_handler)
+LOGGER.addHandler(cw_handler)
 
 encoded_json = os.environ.get("FIREBASE_SETTINGS")
 json_data = json.loads(encoded_json)
@@ -20,6 +40,12 @@ app = FastAPI(
     version="1.0",
     root_path="/develop",
 )
+
+@app.middleware("http")
+async def log_request(request, call_next):
+    LOGGER.info(f"Request received: {request.method} {request.url}")
+    response = await call_next(request)
+    return response
 
 app.include_router(auth_router)
 app.add_middleware(PyInstrumentMiddleWare)
