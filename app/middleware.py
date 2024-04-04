943980ae21c43c1956ce7762a75849147353a379
pmas98 <pmas98@hotmail.com>
2024-04-04 13:34:05 -0300

    Update middleware and logging configuration

--- app/middleware.py
+++ app/middleware.py
@@ -2,6 +2,45 @@ from pyinstrument import Profiler
 from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 from starlette.requests import Request
 from starlette.responses import HTMLResponse
+import dotenv
+import os
+import watchtower
+import logging
+from time import strftime
+import boto3
+
+dotenv.load_dotenv()
+
+boto3.Session(
+    region_name=os.environ.get("AWS_DEFAULT_REGION"),
+    aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
+)
+
+def setup_logging():
+    LOGGER = logging.getLogger(__name__)
+    LOGGER.setLevel(logging.INFO)
+    console_handler = logging.StreamHandler()
+    cw_handler = watchtower.CloudWatchLogHandler(
+        log_group="identity",
+        stream_name=strftime("%Y-%m-%d-%H-%M-%S")
+    )
+    LOGGER.addHandler(console_handler)
+    LOGGER.addHandler(cw_handler)
+
+    ERROR_LOGGER = logging.getLogger('error_logger')
+    ERROR_LOGGER.setLevel(logging.ERROR)
+
+    error_console_handler = logging.StreamHandler()
+    ERROR_LOGGER.addHandler(error_console_handler)
+
+    error_cw_handler = watchtower.CloudWatchLogHandler(
+        log_group="identity-errors",
+        stream_name=strftime("%Y-%m-%d-%H-%M-%S")
+    )
+    ERROR_LOGGER.addHandler(error_cw_handler)
+
+    return LOGGER, ERROR_LOGGER
 
 class PyInstrumentMiddleWare(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> HTMLResponse:
--- main.py
+++ main.py
@@ -1,51 +1,43 @@
-from fastapi import FastAPI
+import string
+from fastapi import FastAPI, HTTPException
 from app.routers.auth import router as auth_router
 from firebase_admin import credentials
 import firebase_admin
-from app.middleware import PyInstrumentMiddleWare
+from app.middleware import PyInstrumentMiddleWare, setup_logging
 import dotenv
 import os
 import json
-import watchtower
-import logging
-from time import strftime
-import boto3
+from starlette.concurrency import iterate_in_threadpool
 
 dotenv.load_dotenv()
 
-boto3.Session(
-    region_name=os.environ.get("AWS_DEFAULT_REGION"),
-    aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
-)
-
-LOGGER = logging.getLogger(__name__)
-LOGGER.setLevel(logging.INFO)
-console_handler = logging.StreamHandler()
-cw_handler = watchtower.CloudWatchLogHandler(
-    log_group="identity",
-    stream_name=strftime("%Y-%m-%d-%H-%M-%S")
-)
-LOGGER.addHandler(console_handler)
-LOGGER.addHandler(cw_handler)
+LOGGER, ERROR_LOGGER = setup_logging()
 
 encoded_json = os.environ.get("FIREBASE_SETTINGS")
 json_data = json.loads(encoded_json)
 cred = credentials.Certificate(json_data)
 firebase_admin.initialize_app(cred)
-
 app = FastAPI(
-    title="Identity",
-    description="Valid8 Identity Service.",
+    title="CICD Pipeline",
+    description="Service for an app.",
     version="1.0",
     root_path="/develop",
 )
 
 @app.middleware("http")
 async def log_request(request, call_next):
+
     LOGGER.info(f"Request received: {request.method} {request.url}")
+
     response = await call_next(request)
+    response_body = [section async for section in response.body_iterator]
+    response.body_iterator = iterate_in_threadpool(iter(response_body))
+    LOGGER.info(f"Request received: {request.method} {request.url} , response_body={response_body[0].decode()}")
+    if response.status_code >= 400:
+        ERROR_LOGGER.error(f"Error: {response.status_code}, response_body={response_body[0].decode()}")
+
     return response
 
 app.include_router(auth_router)
+
 app.add_middleware(PyInstrumentMiddleWare)
